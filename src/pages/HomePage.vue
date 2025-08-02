7f210ff37db41b7be8f4bd15774be403587f9ecb
gushen610140 <w56038009@outlook.com>
2025-08-02 23:32:25 +0800

    feat: basic three

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,61 @@
 <script lang="ts" setup>
-// Home page component
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { onMounted, ref } from "vue";
+
+const canvasContainer = ref<HTMLDivElement | null>(null);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const floorSize = 40;
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load("/src/assets/floor-texture.png");
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.NearestFilter;
+texture.colorSpace = THREE.SRGBColorSpace;
+const repeats = floorSize / 2;
+texture.repeat.set(repeats, repeats);
+
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 5, 0);
+controls.update();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+onMounted(() => {
+  if (!canvasContainer.value) return;
+
+  canvasContainer.value.appendChild(renderer.domElement);
+
+  const animate = () => {
+    renderer.render(scene, camera);
+    controls.update();
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  };
+  renderer.setAnimationLoop(animate);
+});
 </script>
 
 <template>
   <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">首页</h1>
-        <p class="py-6">欢迎使用自动路由系统</p>
-      </div>
-    </div>
+    <div ref="canvasContainer"></div>
   </div>
 </template>
 
